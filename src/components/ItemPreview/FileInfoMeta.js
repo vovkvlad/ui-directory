027bb2bf5fbab82b7abb95a78579c2f8a3d146c8
vovkvlad <VKolo.bokV@gmail.com>
2018-09-17 23:32:51 +0300

    Finished File Meta

--- src/components/ItemPreview/FileInfoMeta.js
+++ src/components/ItemPreview/FileInfoMeta.js
@@ -2,4 +2,9 @@ import React from 'react';
 
 import styles from './ItemPreview.scss';
 
-export default props => <span className={styles.itemMeta}>{JSON.stringify(props.meta, null, 2)}</span>
+export default props => (
+  <div className={styles.itemMeta}>
+    <h3>Details:</h3>
+    <pre>{JSON.stringify(props.meta, null, 2)}</pre>
+  </div>
+);
