639afb403538fe91dd4f4375ee1090dee004f029
vovkvlad <VKolo.bokV@gmail.com>
2019-04-02 22:32:48 +0300

    - added support for class decorators - started mobx app

---
+++ src/apps/mobx/AppDecorator.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import { difference } from 'lodash';
+import { observer } from 'mobx-react';
+
+// import AppController from './AppController';
+export default class AppDecorator extends Component {
+  getDirectoryPath = ({ basePath, actualPath }) => {
+    const basePathArray = basePath.split('/');
+    const actualPathArray = actualPath.split('/');
+    
+    return '/'.concat(difference(actualPathArray, basePathArray).join('/'));
+  };
+  
+  render() {
+    return (
+      <Route render={routerProps => {
+        const { location: { pathname }, match: { url } } = routerProps;
+        const appRoot = this.getDirectoryPath({ basePath: url, actualPath: pathname });
+        
+        return <div>AAAAAAA</div>;
+      }}/>
+    );
+  }
+}
--- src/apps/mobx/index.js
+++ src/apps/mobx/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
-
-import mobxLogo from 'assets/mobx.png';
+import AppDecorator from './AppDecorator';
 
 const mobxApp = () => {
   return (
-    <div>
-      <img src={mobxLogo} height="100" width="100"/>
-      <p>Comming soon</p>
-    </div>
+    <AppDecorator />
   );
 };
 
